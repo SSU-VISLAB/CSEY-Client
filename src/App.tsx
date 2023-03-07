de1327efcb89a5d30a20f86512e1ef4c86434ebb
ptyoiy <56474564+ptyoiy@users.noreply.github.com>
2023-03-07 19:01:15 +0900

    Merge branch 'dev' of https://github.com/SSU-VISLAB/CSEY-Client into feat/bottomMenu

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import BottomNavbar from "./components/BottomNavbar";
 import { Div } from "./AppStyles";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 import EventModal from "./components/EventModal";
 import MainPage from "./pages/MainPage";
 
