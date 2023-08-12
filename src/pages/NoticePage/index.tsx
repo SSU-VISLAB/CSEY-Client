95f08bc6dcac0495ff9050860c67e790b1947aab
j-nary <dlwls0921@naver.com>
2023-08-13 01:51:57 +0900

    refactor: branch test

--- src/pages/NoticePage/index.tsx
+++ src/pages/NoticePage/index.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import HeaderLogo from "../../components/HeaderLogo";
 import * as s from "./styles";
 
+interface AlertInterface {
+  
+}
 const NoticePage = () => {
+  const [alerts, setAlerts] = useState<AlertInterface[]>([]);
   return (
     <s.Wrapper>
       <HeaderLogo/>
