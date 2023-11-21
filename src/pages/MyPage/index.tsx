66f5aef170c159fc8f3869be0a4601ecadf3b4c0
ptyoiy <56474564+ptyoiy@users.noreply.github.com>
2023-11-22 07:28:28 +0900

    feat: 알림 설정 페이지 구조 클래스화
    알림 설정의 모든 내용을 클래스화 해서 관리
    MyPage\index.tsx는 위 인스턴스의 데이터를 표시하는 용도
    
    성능 최적화를 위해 index.tsx의 TODO 진행해야 함

--- src/pages/MyPage/index.tsx
+++ src/pages/MyPage/index.tsx
@@ -6,6 +6,7 @@ import { kakaoLogout } from "../../axios";
 import * as BottomNav from "../../components/BottomNavbar";
 import HeaderLogo from "../../components/HeaderLogo";
 import { Login } from "../../components/Login";
+import { AlarmSettingContents } from "../../context/setting";
 import { getAlarmsQuery, setAlarmMutation } from "../../query/alarm";
 import { getUserInfoQuery, loginQuery } from "../../query/user";
 import {
@@ -32,7 +33,7 @@ export interface SettingCardContents {
   meta: string;
   description?: string;
   eventHandler?: (e?) => void;
-  value?: any
+  value?: any;
 }
 
 export interface AlarmSettingCardContents extends SettingCardContents {
@@ -46,18 +47,6 @@ export type SettingCardData<T extends SettingCardContents> = {
   hasLogin?: boolean;
 };
 
-const mapper = {
-  "전체 알림 끄기": 'alarm_push',
-  "행사 알림 켜기": 'event_push',
-  "알림 시간": 'events_timer',
-  "알림 형태": 'events_form',
-  "행사 등록 알림 켜기": 'events_post',
-  "일정 당일 알림 켜기": 'major_schedule_push',
-  "일정 등록 알림 켜기": 'major_schedule_post',
-  '일반 공지 알림 켜기': 'notice_push',
-  '긴급 공지 알림 켜기': 'alerts_push'
-}
-
 /** My - 설정페이지 */
 const Setting = () => {
   loginQuery();
@@ -66,35 +55,58 @@ const Setting = () => {
   const navigate = useNavigate();
   const client = useQueryClient();
 
-  const settingContents = useMemo(() => ([
-    { header: { title: userData?.name || '로그인 해주세요', icon: User } },
-    userData && {
-      header: { title: "Account", icon: Settings },
-      content: [
-        { meta: "학적 정보", description: userData?.major || "학적 정보 미등록" },
-        { meta: "로그아웃", eventHandler: () => (alert('로그아웃'), kakaoLogout(client, navigate)) },
-        { meta: "계정 탈퇴" },
-      ],
-    },
-    {
-      header: { title: "Settings", icon: Settings },
-      content: [
-        { meta: "테마", description: "시스템 기본값" },
-        userData && { meta: "알림 설정", eventHandler: () => navigate('/My/Alarm') },
-      ].filter(v => v),
-    },
-    {
-      header: { title: "Guide", icon: Settings },
-      content: [
-        { meta: "앱 버전", description: "v.10.0", eventHandler: () => alert('앱 버전 클릭!') },
-        { meta: "문의하기", description: "nabest@vis.ssu.ac.kr" },
-        { meta: "개발자 정보" },
-        { meta: "서비스 이용 약관" },
-        { meta: "개인정보 처리방침" },
-        { meta: "오픈소스 라이센스" },
-      ],
-    },
-  ] as SettingCardData<SettingCardContents>[]).filter(v => v), [userData]);
+  const settingContents = useMemo(
+    () =>
+      (
+        [
+          {
+            header: { title: userData?.name || "로그인 해주세요", icon: User },
+          },
+          userData && {
+            header: { title: "Account", icon: Settings },
+            content: [
+              {
+                meta: "학적 정보",
+                description: userData?.major || "학적 정보 미등록",
+              },
+              {
+                meta: "로그아웃",
+                eventHandler: () => (
+                  alert("로그아웃"), kakaoLogout(client, navigate)
+                ),
+              },
+              { meta: "계정 탈퇴" },
+            ],
+          },
+          {
+            header: { title: "Settings", icon: Settings },
+            content: [
+              { meta: "테마", description: "시스템 기본값" },
+              userData && {
+                meta: "알림 설정",
+                eventHandler: () => navigate("/My/Alarm"),
+              },
+            ].filter((v) => v),
+          },
+          {
+            header: { title: "Guide", icon: Settings },
+            content: [
+              {
+                meta: "앱 버전",
+                description: "v.10.0",
+                eventHandler: () => alert("앱 버전 클릭!"),
+              },
+              { meta: "문의하기", description: "nabest@vis.ssu.ac.kr" },
+              { meta: "개발자 정보" },
+              { meta: "서비스 이용 약관" },
+              { meta: "개인정보 처리방침" },
+              { meta: "오픈소스 라이센스" },
+            ],
+          },
+        ] as SettingCardData<SettingCardContents>[]
+      ).filter((v) => v),
+    [userData],
+  );
   return (
     <SettingList>
       <HeaderLogo />
@@ -103,17 +115,23 @@ const Setting = () => {
           key={i}
           header={data.header}
           content={data?.content}
-          hasLogin={!i && !(userData)}
+          hasLogin={!i && !userData}
         />
       ))}
-    </SettingList >
+    </SettingList>
   );
 };
 
-const ItemList = ({ header, content, hasLogin }: SettingCardData<SettingCardContents>) => {
+const ItemList = ({
+  header,
+  content,
+  hasLogin,
+}: SettingCardData<SettingCardContents>) => {
   return (
     <Group elevation={4}>
-      <ItemHeader title={header.title} HeaderIcon={header.icon} bold>{hasLogin && <Login />}</ItemHeader>
+      <ItemHeader title={header.title} HeaderIcon={header.icon} bold>
+        {hasLogin && <Login />}
+      </ItemHeader>
       {content && (
         <ContentGroup>
           {content.map((v, i) => (
@@ -139,50 +157,20 @@ const AlarmSetting = () => {
   const navigate = useNavigate();
   const client = useQueryClient();
   const setAlarm = setAlarmMutation(client);
-  const alarmSettingContents = useMemo<SettingCardData<AlarmSettingCardContents>[]>(() => alarms.isSuccess && [
-    {
-      header: { title: "전체 알림" },
-      content: [{
-        hasCheck: true, meta: "전체 알림 끄기", value: alarmData[mapper["전체 알림 끄기"]], eventHandler(e) {
-          setAlarm.mutate({ [mapper["전체 알림 끄기"]]: e.target.checked } as any);
-        },
-      }],
-    },
-    {
-      header: { title: "행사 알림" },
-      content: [
-        {
-          hasCheck: true, meta: "행사 알림 켜기", value: alarmData[mapper['행사 알림 켜기']],
-          child: [
-            { meta: "알림 시간", description: "시작 24시간 전", eventHandler: () => alert("알림 시간 설정") },
-            { meta: "알림 형태", description: "북마크만", eventHandler: () => alert("알림 형태 설정") },
-          ],
-        },
-        { hasCheck: true, meta: "행사 등록 알림 켜기", value: alarmData[mapper['행사 등록 알림 켜기']] }
-      ],
-    },
-    {
-      header: { title: '학사일정 알림' },
-      content: [
-        { hasCheck: true, meta: "일정 당일 알림 켜기", value: alarmData[mapper['일정 당일 알림 켜기']] },
-        { hasCheck: true, meta: "일정 등록 알림 켜기", value: alarmData[mapper['일정 등록 알림 켜기']] },
-      ]
-    },
-    {
-      header: { title: '공지 알림' },
-      content: [
-        { hasCheck: true, meta: '일반 공지 알림 켜기', value: alarmData[mapper['일반 공지 알림 켜기']] },
-        { hasCheck: true, meta: '긴급 공지 알림 켜기', value: alarmData[mapper['긴급 공지 알림 켜기']] },
-      ]
-    }
-  ], [alarms.data])
-  if (alarms.isPending) return "loading...";
+  if (!alarmData) return "loading...";
+  console.time('contents')
+  const alarmContents = new AlarmSettingContents(alarmData, setAlarm).getContents();
+  console.timeEnd('contents')
+  // TODO: alarmContent class에 메서드 추가
+  // setAlarmData, setMutation => 설정 시 value와 eventHandler 추가
+  // AlarmSettingList 컴포넌트에서 메서드 호출
+  console.log({ alarmContents })
   return (
     <SettingList>
       <HeaderLogo />
       <ContentRow>
         <Icon>
-          <ArrowLeft size={24} onClick={() => navigate('/My')} />
+          <ArrowLeft size={24} onClick={() => navigate("/My")} />
         </Icon>
         <Icon>
           <Settings size={24} />
@@ -192,32 +180,38 @@ const AlarmSetting = () => {
         </Title>
       </ContentRow>
 
-      {alarmSettingContents.map((data, i) => (
+      {alarmContents.map((alarmGroup, i) => (
         <AlarmSettingList
           key={i}
-          header={data.header}
-          content={data?.content}
+          header={alarmGroup.header}
+          content={alarmGroup?.content}
         ></AlarmSettingList>
       ))}
     </SettingList>
   );
 };
 
-const AlarmSettingList = ({ header, content }: SettingCardData<AlarmSettingCardContents>) => {
+const AlarmSettingList = ({
+  header,
+  content,
+}: SettingCardData<AlarmSettingCardContents>) => {
   content = content.flatMap((v) => (v.child ? [v, ...v.child] : v));
   return (
     <Group elevation={4}>
       <ItemHeader
         title={header.title}
-        HeaderIcon={undefined}
-        padding={false}
-        bold={true}
+        bold
       />
       <ContentGroup>
         {content.map((v) => {
           if (v.hasCheck)
             return (
-              <ContentCheck eventHandler={v.eventHandler} meta={v.meta} key={v.meta} value={v.value} />
+              <ContentCheck
+                eventHandler={v.eventHandler}
+                meta={v.meta}
+                key={v.meta}
+                value={v.value}
+              />
             );
           else
             return (
@@ -240,10 +234,10 @@ const ItemHeader = ({
   HeaderIcon,
   padding,
   bold,
-  children
+  children,
 }: {
   title: string;
-  HeaderIcon: BottomNav.Icon;
+  HeaderIcon?: BottomNav.Icon;
   padding?: boolean;
   bold: boolean;
   children?;
@@ -260,7 +254,11 @@ const ItemHeader = ({
 };
 
 /** 체크버튼 있는 단일 요소인 alarm설정 */
-const ContentCheck = ({ eventHandler, meta, value }: AlarmSettingCardContents) => {
+const ContentCheck = ({
+  eventHandler,
+  meta,
+  value,
+}: AlarmSettingCardContents) => {
   const sx = {
     "& .MuiSvgIcon-root": {
       fontSize: "2.5vh",
@@ -282,7 +280,11 @@ const ContentCheck = ({ eventHandler, meta, value }: AlarmSettingCardContents) =
 };
 
 /** 자식 요소인 alarm설정 */
-const ContentChild = ({ eventHandler, meta, description }: AlarmSettingCardContents) => {
+const ContentChild = ({
+  eventHandler,
+  meta,
+  description,
+}: AlarmSettingCardContents) => {
   return (
     <ContentRow>
       <Arrow />
