00ad779b69b1e0c2e2bbf92a25728f3ca1f8a437
ptyoiy <56474564+ptyoiy@users.noreply.github.com>
2023-12-13 06:22:18 +0900

    Revert "Revert "Merge branch 'feat/api' into feat/login""
    This reverts commit 1e73f0a924480ffa58838bbeb316586f4f6464b8.

--- src/query/alarm.ts
+++ src/query/alarm.ts
@@ -1,5 +1,4 @@
-import { QueryClient } from '@tanstack/query-core';
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { getAlarms } from "../axios";
 import { setAlarm } from "../axios/alarm";
 import { IAlarm } from "../context/user";
@@ -19,14 +18,18 @@ export const getAlarmsQuery = (id: number) => {
   return info;
 }
 
-export const setAlarmMutation = (queryClient: QueryClient) => {
+export const setAlarmMutation = () => {
+  const queryClient = useQueryClient();
   const setAlarmMutation = useMutation({
     mutationFn: setAlarm,
-    onSuccess(data, variables, context) {
-      const alarms = queryClient.getQueryData<IAlarm>(['getAlarms'])
-      const updated = queryClient.setQueryData<IAlarm>(['getAlarms'], { ...alarms, ...variables });
-      // queryClient.invalidateQueries({ queryKey: ["getAlarms"] });
+    onMutate(nextAlarm) {
+      const prevAlarms = queryClient.getQueryData<IAlarm>(['getAlarms'])
+      const nextAlarms = queryClient.setQueryData<IAlarm>(['getAlarms'], { ...prevAlarms, ...nextAlarm });
+      return { prevAlarms, nextAlarms };
     },
+    onError(error, nextAlarm, context) {
+      queryClient.setQueryData<IAlarm>(['getAlarms'], context.prevAlarms);
+    }
   })
   return setAlarmMutation;
 }
