208185e6dfa62cc1c005164397e385ad284f27af
J nary <dlwls0921@naver.com>
2024-02-29 20:27:19 +0900

    refactor: AlertBanner

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,6 +1,3 @@
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import AlertSrc from "../../assets/Icons/MainAlertIcon.png";
 import HeaderLogo from "../../components/HeaderLogo";
 import { RCalendar } from "./Rcalendar";
 import * as s from "./styles";
@@ -8,50 +5,16 @@ import EventCardCarousel from "../../components/Carousel";
 import EventCard from "../../components/EventCard";
 import { getEventsQuery } from "../../api/query/event";
 import { getAlertsQuery } from "../../api/query/alert";
-import { AlertType } from "../../types";
+import AlertBanner from "../../components/AlertBanner";
 
 const MainPage = () => {
   const { data: alerts, isPending: isAlertsPending } = getAlertsQuery();
   const { data: events, isPending: isEventsPending } = getEventsQuery();
-  const [showRedDot, setShowRedDot] = useState<boolean>(true);
-  const [currentAlertIndex, setCurrentAlertIndex] = useState(0);
-
-  useEffect(() => {
-    let interval;
-    if (!isAlertsPending) {
-      interval = setInterval(() => {
-        setCurrentAlertIndex(
-          (prevIndex) => (prevIndex + 1) % (alerts as AlertType[])?.length,
-        );
-      }, 5000);
-    }
-    return () => clearInterval(interval);
-  }, []);
   console.log({ events });
   return (
     <s.MainList>
       <HeaderLogo />
-      <s.Group>
-        <s.Header>
-          <s.Icon src={AlertSrc} alt="Alert" />
-          {!isAlertsPending && alerts && 
-            (alerts.length === 0 ? (
-              <s.Meta>지금은 중요 공지가 없습니다.</s.Meta>
-            ) : (
-              <s.StyledLink to="/Notification">
-                <s.AlertList key={currentAlertIndex}>
-                  {alerts[currentAlertIndex].title}
-                </s.AlertList>
-              </s.StyledLink>
-            ))}
-          <Link to="/Notification">
-            <s.More>
-              {" "}
-              {showRedDot && <s.Activate />} {"더보기 >"}
-            </s.More>
-          </Link>
-        </s.Header>
-      </s.Group>
+      <AlertBanner isAlertsPending={isAlertsPending} alerts={alerts}/>
       {!isEventsPending && (<EventCardCarousel>
         {events?.map((event) => (
           <EventCard key={event.id} event={event} />
