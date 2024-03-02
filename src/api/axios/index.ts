36b1fa3a75fb9dc92486a81d1c042ba8caff2c8b
ptyoiy <dltjr0036@gmail.com>
2024-03-02 14:18:54 +0900

    fix: set axios base url to deployed server

--- src/api/axios/index.ts
+++ src/api/axios/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { getUserInfo, kakaoLogout, login } from "./user";
 
-axios.defaults.baseURL = "http://localhost:7070/api";
+axios.defaults.baseURL = "http://203.253.21.193:7070/api";
 export { getUserInfo, kakaoLogout, login };
 
